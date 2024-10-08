4b0dcc42bac6f1ef3018452085658364ba8481fd
satyadeep roat <satyadeeproat@satyadeeps-MacBook-Pro.local>
2024-10-08 12:00:40 +0200

    feat: Test config

--- publish-release.sh
+++ publish-release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Check if an argument is provided
 if [ "$#" -ne 1 ]; then
@@ -9,7 +10,7 @@ fi
 release_type=$1
 
 commit_message=$(git log --format=oneline --pretty=format:%s -n 1 $CIRCLE_SHA1)
-latest_tag=$(gh release view --json tagName --jq '.tagName')
+latest_tag=$(gh release list --limit 1 --json tagName --jq '.[0].tagName // "v0.0.0"')
 
 echo "Commit message: $commit_message" 
 echo "Latest tag: $latest_tag"
@@ -25,8 +26,11 @@ increment_version() {
   # Check input string for [major], [minor], [patch]
   if [[ $input == *"[major]"* ]]; then
     major=$((major + 1))
+    minor=0
+    patch=0
   elif [[ $input == *"[minor]"* ]]; then
     minor=$((minor + 1))
+    patch=0
   else
     # Treat as [patch] if no [major] or [minor] found
     patch=$((patch + 1))
@@ -38,12 +42,11 @@ increment_version() {
   echo "$new_version"
 }
 
-# Get new version
 cmd_args=""
 
 if [ "$release_type" = "staging" ]; then
-  new_version="$staging-$CIRCLE_SHA1"
-  cmd_args="--draft --prerelease"
+  new_version="$latest_tag-staging-$CIRCLE_SHA1"
+  cmd_args="--prerelease"
 elif [ "$release_type" = "production" ]; then
   if [[ $CIRCLE_BRANCH != "master" ]]; then
     echo "Production releases should only be created from the master branch"
@@ -55,6 +58,17 @@ else
   exit 1
 fi
 
-release_url=$(gh release create "$new_version" ./providers/*.jar --title="$new_version" --notes="$commit_message" $cmd_args)
+# Check if providers directory exists and contains JAR files
+if [ ! -d "./providers" ] || [ -z "$(ls -A ./providers/*.jar 2>/dev/null)" ]; then
+    echo "Error: providers directory is missing or contains no JAR files."
+    exit 1
+fi
 
-echo $release_url
+release_notes="$commit_message"
+if [ "$release_type" = "staging" ]; then
+    release_notes="Staging Release: $release_notes"
+fi
+
+release_url=$(gh release create "$new_version" ./providers/*.jar --title="$new_version" --notes="$release_notes" $cmd_args)
+
+echo "Release created: $release_url"
